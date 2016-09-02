caf9a2933f20738af444fd22cb2264ace4660e5d
Taku24 <taku178.24@hotmail.co.jp>
2016-09-02 15:26:18 +0900

    Initial Commit

---
+++ AnimationSwiftOriginal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationSwiftOriginal
+//
+//  Created by TAKU on 2016/09/02.
+//  Copyright © 2016年 TAKU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

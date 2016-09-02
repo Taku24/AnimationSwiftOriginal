acd7caad99a2cf89f3a438cb0c87dbe024945e1d
Taku24 <taku178.24@hotmail.co.jp>
2016-09-02 16:23:12 +0900

    MVCの基礎を実装

--- AnimationSwiftOriginal/ViewController.swift
+++ AnimationSwiftOriginal/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let mainView = MainView()
+    
+    override func loadView() {
+        self.view = mainView
+        self.view.backgroundColor = UIColor.whiteColor()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
